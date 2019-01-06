0cad68b21a259db419572e4e79bfddcbdcff3e64
Den <blackpirates@mail.ru>
2019-01-06 21:45:11 +0300

    Добавил gulp

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const {src,task,dest,watch} = require("gulp");
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const rename = require("gulp-rename");
+const terser = require('gulp-terser');
+
+
+function cssTask(){
+    src("./css/style.css")
+    .pipe(autoprefixer())
+    .pipe(rename("style.autoprefixer.css"))
+    .pipe(dest("./css/"))
+    .pipe(cleanCSS())
+    .pipe(rename("style.min.css"))
+    .pipe(dest("./css/"));
+}
+function jsTask(){
+    src("./js/script.js")
+    .pipe(terser())
+    .pipe(rename("script.min.js"))
+    .pipe(dest("./js/"))
+}
+function build(cb){
+    cssTask();
+    jsTask();
+    cb();
+}
+
+task("build",build);
+task("watch",function(cb){
+    watch("./css/style.css",{ ignoreInitial: false }, function css(cb){
+        cssTask();
+        cb();
+    })
+    watch("./js/script.js",{ ignoreInitial: false }, function js(cb){
+        jsTask();
+        cb();
+    })
+    cb();
+})
+
+exports.default = build;
